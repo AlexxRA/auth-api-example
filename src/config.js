2edd95571dc71bc39bb407216738536e4f0c83fd
Alejandro Rosales <rosales.armas98@gmail.com>
2022-09-12 15:21:48 -0500

    files

---
+++ src/config.js
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+config();
+
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/local";
+export const PORT = process.env.PORT || 4000;
+export const SECRET = "my best secret id";
+
+export const ADMIN_EMAIL = process.env.ADMIN_EMAIL || "admin@localhost";
+export const ADMIN_USERNAME = process.env.ADMIN_USERNAME || "admin";
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "admin";
